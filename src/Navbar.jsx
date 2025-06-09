33d7f2c3fd3a0e0fb7b4128fbfcc9ed0115a4947
saurabh.225287101 <saurabh.225287101@vcet.edu.in>
2025-06-09 18:53:06 +0000

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="h-21 w-full bg-[ivory] absolute top-0 left-0">
+        <div className="h-21 w-47  bg-[orange] " >
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
