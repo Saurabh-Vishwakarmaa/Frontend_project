2fbc3570d45b59afaa56eed249807001d445a22d
saurabh.225287101 <saurabh.225287101@vcet.edu.in>
2025-06-10 19:08:08 +0000

    nav-complete

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
+
 function Navbar() {
   return (
-    <div className="h-21 w-full bg-[ivory] absolute top-0 left-0">
-        <div className="h-21 w-47  bg-[orange] " >
+    <div className="h-21 w-[100vw] bg-[ivory] absolute top-0 left-0 flex ">
+        <div className="h-21 w-97 font-sans text-md flex justify-around items-center px-13 text-black " >
+           <a  href = "#">Services</a>
+           <a href = "#">Work</a>
+           <a href = "#" className="text-black">About</a>
+           
 
         </div>
-        <div>
 
+        <div className="h-21 w-190  flex justify-around items-center px-50 text-2xl text-black font-bold">
+          <div className="relative left-3 top-2 h-15 w-15 ">
+            <img src="src/assets/logo.png" alt="" srcset="" />
+          </div>
+          
+        <div className="relative left-[-85px]">
+          Astratto
         </div>
-        <div>
-
+          
+        </div>
+        <div className="h-21 w-97  flex justify-around items-center px-13">
+    <a href="#">Careers</a>
+    <a href="#">Contact Us</a>
         </div>
 
     </div>
