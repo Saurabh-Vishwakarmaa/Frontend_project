fa07cbac785aa5bee25c7341040085c7c90a0920
saurabh.225287101 <saurabh.225287101@vcet.edu.in>
2025-06-11 17:51:29 +0000

    the hero-starts-here

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Navbar'
+import Hero from './Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,7 @@ function App() {
   return (
     <>
     <Navbar/>
-      Hello World
-      here we have sometime
+      <Hero/>
     </>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='h-[90vh] w-[100vw] bg-[ivory] absolute left-0 top-21 '>
+
+        <div className='relative top-15 w-105 left-[35%] bg-white h-25 text-black text-roboto text-[21px]'>
+            Igniting the spark of Inspiration
+
+        </div>
+        <div className='  justify-center items-center relative top-15 w-260 left-[15%] bg-white h-105 text-black text-roboto text-[105px] text-center font-bold'>
+            Unleash Your 
+            <br></br>Brand <img src="src/assets/orange-logo.png" alt="" srcset="" /> with Our Magico Design
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
